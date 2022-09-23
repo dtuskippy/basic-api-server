0bea5b3d441fda05f298b32eb63067db6f0d9e6d
Alan Chelko <achelko@gmail.com>
2022-09-23 10:42:15 +0000

    Completed all REST API tests

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -16,31 +16,147 @@ afterAll(async () => {
 });
 
 
-describe('Testing REST API', () => {
+describe('Testing REST API for drinks', () => {
 
   test('Create a drinks', async() => {
-    let response = await request.post('/drinks').send({
+    let response1 = await request.post('/drinks').send({
       brand: 'tester',
       category: 'wine',
       alcohol: 'non-alcohol',
     });
 
-    expect(response.status).toEqual(200);
+    let response2 = await request.post('/drinks').send({
+      brand: 'tester2',
+      category: 'beer',
+      alcohol: 'alcohol',
+    });
+
+    expect(response1.status).toEqual(200);
+    expect(response1.body.brand).toEqual('tester');
+    expect(response1.body.category).toEqual('wine');
+    expect(response1.body.alcohol).toEqual('non-alcohol');
+    expect(response2.status).toEqual(200);
+    expect(response2.body.brand).toEqual('tester2');
+    expect(response2.body.category).toEqual('beer');
+    expect(response2.body.alcohol).toEqual('alcohol');
+  });
+
+
+  test('Reads all drinks', async () => {
+    let response = await request.get('/drinks');
+    console.log('Should have two records', response.body);
+    expect(response.body.length).toBe(2);
+    expect(response.body[0].brand).toEqual('tester');
+    expect(response.body[0].category).toEqual('wine');
+    expect(response.body[0].alcohol).toEqual('non-alcohol');
+    expect(response.body[1].brand).toEqual('tester2');
+    expect(response.body[1].category).toEqual('beer');
+    expect(response.body[1].alcohol).toEqual('alcohol');
+
+  });
+
+  test('Reads a single drink', async () => {
+    let response = await request.get('/drinks/1');
+
     expect(response.body.brand).toEqual('tester');
     expect(response.body.category).toEqual('wine');
     expect(response.body.alcohol).toEqual('non-alcohol');
   });
 
-  test('Should read from drinks', () => {
-    expect(true).toBe(false);
+  test('Updates a drink', async () => {
+    let response = await request.put('/drinks/1').send({
+      brand: 'tester',
+      category: 'beer',
+      alcohol: 'non-alcohol',
+    });
+
+    expect(response.body.brand).toEqual('tester');
+    expect(response.body.category).toEqual('beer');
+    expect(response.body.alcohol).toEqual('non-alcohol');
   });
 
-  test('Should update drinks', () => {
-    expect(true).toBe(false);
+
+  test('Should delete a drinks', async () => {
+    await request.delete('/drinks/1');
+    let response = await request.get('/drinks');
+    console.log('Should have one record', response.body);
+
+    expect(response.body.length).toBe(1);
+    expect(response.body[0].brand).toEqual('tester2');
+    expect(response.body[0].category).toEqual('beer');
+    expect(response.body[0].alcohol).toEqual('alcohol');
   });
 
-  test('Should delete a drinks', () => {
-    expect(true).toBe(false);
-  });
-  
 });
+
+describe('Testing REST API for meat', () => {
+
+  test('Create a meat', async() => {
+    let response1 = await request.post('/meat').send({
+      name: 'tester',
+      category: 'pork',
+      freshness: 'fresh',
+    });
+
+    let response2 = await request.post('/meat').send({
+      name: 'tester2',
+      category: 'beef',
+      freshness: 'frozen',
+    });
+
+    expect(response1.status).toEqual(200);
+    expect(response1.body.name).toEqual('tester');
+    expect(response1.body.category).toEqual('pork');
+    expect(response1.body.freshness).toEqual('fresh');
+    expect(response2.status).toEqual(200);
+    expect(response2.body.name).toEqual('tester2');
+    expect(response2.body.category).toEqual('beef');
+    expect(response2.body.freshness).toEqual('frozen');
+  });
+
+
+  test('Reads all meat', async () => {
+    let response = await request.get('/meat');
+    console.log('Should have two records', response.body);
+    expect(response.body.length).toBe(2);
+    expect(response.body[0].name).toEqual('tester');
+    expect(response.body[0].category).toEqual('pork');
+    expect(response.body[0].freshness).toEqual('fresh');
+    expect(response.body[1].name).toEqual('tester2');
+    expect(response.body[1].category).toEqual('beef');
+    expect(response.body[1].freshness).toEqual('frozen');
+  });
+
+  test('Reads a single meat', async () => {
+    let response = await request.get('/meat/1');
+    expect(response.body.name).toEqual('tester');
+    expect(response.body.category).toEqual('pork');
+    expect(response.body.freshness).toEqual('fresh');
+  });
+
+  test('Updates a meat', async () => {
+    let response = await request.put('/meat/1').send({
+      name: 'tester',
+      category: 'fish',
+      freshness: 'fresh',
+    });
+
+    expect(response.body.name).toEqual('tester');
+    expect(response.body.category).toEqual('fish');
+    expect(response.body.freshness).toEqual('fresh');
+  });
+
+
+  test('Should delete a meat', async () => {
+    await request.delete('/meat/1');
+    let response = await request.get('/meat');
+    console.log('Should have one record', response.body);
+
+    expect(response.body.length).toBe(1);
+    expect(response.body[0].name).toEqual('tester2');
+    expect(response.body[0].category).toEqual('beef');
+    expect(response.body[0].freshness).toEqual('frozen');
+  });
+
+});
+
